20e8d03108374b194eb010ee94658b6885fc6f20
moelqovs <moelqovs4@gmail.com>
2023-10-13 20:56:50 -0300

    arrow functions: object infCurso, cartHTML, cleanCart, cartHTML

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const carrito = document.querySelector('#carrito')
 const listaCursos = document.querySelector('#lista-cursos')
 const listaCarrito = document.querySelector('#lista-carrito tbody')
 const vaciarCarrito = document.querySelector('#vaciar-carrito')
-
+let articulosCarrito = []
 
 
 //listeners carrito
@@ -31,15 +31,52 @@ const agregarCurso = (e) => {
 
 //2. leer elemento al cual se activo el evento click y extraer informacion
 const leerDatosCurso = (curso) => {
-    console.log(curso)
 
     const infoCurso = {
         imagen: curso.querySelector('img').src,
         titulo: curso.querySelector('h4').textContent,
         precio: curso.querySelector('.precio span').textContent,
-        id: curso.querySelector('a').getAtribute('data-id'),
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
     }
 
+    //Agregar elementos del dato curso al carrito
+    articulosCarrito = [...articulosCarrito,infoCurso]
+    carritoHTML()
+    console.log(articulosCarrito)
+}
+
+//3. se muestra el html basado en el carrito de compras
+const carritoHTML = () => {
+    //limpiar el html
+    limpiarHTML()
+
+    //recorre el carrito y genera el html
+    articulosCarrito.forEach ( curso => {
+        const {imagen, titulo, precio, cantidad, id} = curso
+        const row = document.createElement('tr')
+        row.innerHTML = `
+            <td> <img src='${imagen}' width='100'> </td>
+            <td> ${titulo} </td>
+            <td> ${precio} <td>
+            <td> ${cantidad} </td>
+            <td> <a href="#" class="borrar-curso" data-id="${id}"> x </a> </td>
+        `;
+
+        //Agregar el html del carrito en el tbody
+        listaCarrito.appendChild(row)
+    })
+}
+
+
+//4. eliminar los cursos del tbody
+const limpiarHTML = () => {
+   //una fomra de limpiar es con listaCarrito.innerHTML = ''
+
+   //forma de eliminar mas optimizada
+    while( listaCarrito.firstChild ){
+        listaCarrito.removeChild ( listaCarrito.firstChild )
+    }
 }
 
 
